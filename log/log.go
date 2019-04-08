4a828c1a409db9a680a1abc473cb9c5227d209aa
高广林 <guanglingao@yeah.net>
2019-04-08 12:51:08 +0800

    完善日志

--- log/log.go
+++ log/log.go
@@ -11,9 +11,10 @@ import (
 )
 
 // 文件输出等级，ERROR > WARNING > INFO > DEBUG
+// 大于等于当前配置等级的日志，将被输出到文件
 var Threshold = "INFO"
 
-// 日志文件输出路径，文件夹地址
+// 日志文件输出路径（文件夹地址）
 var Dir = ""
 
 // 日志输出配置初始化
@@ -24,12 +25,12 @@ var info = log.New(os.Stdout, "INFO    ", flag)
 var waring = log.New(os.Stdout, "WARNING ", flag)
 var error = log.New(os.Stdout, "ERROR   ", flag)
 
-var debugAppender = log.New(appendFile(), "DEBUG   ", flag)
-var infoAppender = log.New(appendFile(), "INFO    ", flag)
-var warningAppender = log.New(appendFile(), "WARNING ", flag)
-var errorAppender = log.New(appendFile(), "ERROR   ", flag)
+var debugF = log.New(logTargetFile(), "DEBUG   ", flag)
+var infoF = log.New(logTargetFile(), "INFO    ", flag)
+var warningF = log.New(logTargetFile(), "WARNING ", flag)
+var errorF = log.New(logTargetFile(), "ERROR   ", flag)
 
-// DEBUG
+// Debug output debug level logs
 func Debug(v ...interface{}) {
 	_, p, l, _ := runtime.Caller(1)
 	wd, _ := os.Getwd()
@@ -41,11 +42,11 @@ func Debug(v ...interface{}) {
 	}
 	go debug.Println(v2...)
 	if strings.ToLower(strings.TrimSpace(Threshold)) == "debug" {
-		go debugAppender.Println(v2...)
+		go debugF.Println(v2...)
 	}
 }
 
-// INFO
+// Info output information level logs
 func Info(v ...interface{}) {
 	_, p, l, _ := runtime.Caller(1)
 	wd, _ := os.Getwd()
@@ -58,11 +59,11 @@ func Info(v ...interface{}) {
 	go info.Println(v2...)
 	threshold := strings.ToLower(strings.TrimSpace(Threshold))
 	if threshold == "debug" || threshold == "info" {
-		go infoAppender.Println(v2...)
+		go infoF.Println(v2...)
 	}
 }
 
-// WARNING
+// Warning output warning level logs
 func Warning(v ...interface{}) {
 	_, p, l, _ := runtime.Caller(1)
 	wd, _ := os.Getwd()
@@ -75,11 +76,11 @@ func Warning(v ...interface{}) {
 	go waring.Println(v2...)
 	threshold := strings.ToLower(strings.TrimSpace(Threshold))
 	if threshold == "debug" || threshold == "info" || threshold == "warning" {
-		go warningAppender.Println(v2...)
+		go warningF.Println(v2...)
 	}
 }
 
-// ERROR
+// Error output error level logs
 func Error(v ...interface{}) {
 	_, p, l, _ := runtime.Caller(1)
 	wd, _ := os.Getwd()
@@ -90,10 +91,10 @@ func Error(v ...interface{}) {
 		v2 = append(v2, vv)
 	}
 	go error.Println(v2...)
-	go errorAppender.Println(v2...)
+	go errorF.Println(v2...)
 }
 
-func appendFile() io.Writer {
+func logTargetFile() io.Writer {
 	date := time.Now().Format("2006-01-02")
 	var logFileName string
 	if strings.TrimSpace(Dir) == "" {

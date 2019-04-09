2339f4bcf6a870b8163a97cc0584fff5d00b783a
高广林 <guanglingao@yeah.net>
2019-04-09 19:00:01 +0800

    纠正路径斜线的问题

--- http/controller.go
+++ http/controller.go
@@ -2,22 +2,30 @@ package http
 
 import (
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type Controller struct {
 }
 
-func (c *Controller) GetCookie(name string) string {
-	if cookie, err := handler.Request.Cookie(name); err == nil {
-		return cookie.Value
+// GetCookie 用于读取Cookie中的值
+func (c *Controller) GetCookie(name string) (string, error) {
+	cookie, err := handler.Request.Cookie(name)
+	if err == nil {
+		return cookie.Value, nil
 	}
-	return ""
+	return "", err
 }
 
+// SetCookie 用于设置Cookie值
+// 参数 name: 名称；value: 值；expireAfter: N秒后过期，单位秒
 func (c *Controller) SetCookie(name, value string, expireAfter int) {
+	d, _ := time.ParseDuration(strconv.Itoa(expireAfter) + "s")
 	cookie := &http.Cookie{
 		Name:     name,
 		Value:    value,
+		Expires:  time.Now().Add(d),
 		MaxAge:   expireAfter,
 		HttpOnly: false,
 		SameSite: http.SameSiteStrictMode,
